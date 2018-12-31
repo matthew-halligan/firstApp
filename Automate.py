80cee9347bfed37d6c04019da249197e9af1c4dc
matthew-halligan <42771186+matthew-halligan@users.noreply.github.com>
2018-12-31 17:04:25 -0500

    Add files via upload
    This is a threaded version of the application that allows interaction with gui while long running code performs in the background.  Known bugs include Entry element only providing string elements for default sleep time and default iterations.  WIll need to convert to int in order to function fully

--- Automate.py
+++ Automate.py
@@ -7,7 +7,7 @@ import random
 
 class Automation:
     # initializes an instance of automation.  requires username and password.  All else taken care of by settings
-    def __init__(self, username=None, password=None, browser=None, url=None, iterations=250, comments=[]):
+    def __init__(self, username=None, password=None, browser=None, url=None, timesleep=30, iterations=250, comments=[]):
         self.username = username
         self.password = password
         self.browser = browser
@@ -16,7 +16,12 @@ class Automation:
         self.comments = comments
         #helpers for customization
         self.i = 0 #found in navigation
+        self.j = iterations
+
+        self.timesleep = timesleep #Defaults at 30
+
         self.image_reload_helper = 9
+
 
     # launches the browser, connects to login page, waits, sends keys to login
     def login(self):
@@ -36,9 +41,11 @@ class Automation:
     # Finds like element, waits for full page load, clicks like, goes to next picture, returns control to caller
     def likePost(self):
         likes = self.browser.find_elements_by_class_name("dCJp8")
-        sleep(10)
+        print(len(likes))
+        sleep(self.timesleep)
         likes[0].click()
         sleep(.4)
+        print("I've completed liking the post task")
 
         return
 
@@ -54,7 +61,7 @@ class Automation:
         return
 
     def navigation(self):
-        #navigate to the proper hashtag url after logging in.  Move to 10th image open, check counter, calls appropriate
+        #navigate to the proper hashtag url after logging in.  Move to 10th image open and return control
 
         self.browser.get(self.getUrl())
 
@@ -62,18 +69,7 @@ class Automation:
         images[self.image_reload_helper].click() #starts at 9 will change if page needs reload due to failure
         sleep(2)
         self.i = 0
-        j = self.getIterations()#Number of posts to scroll through.  250 is Default.  Can be changed with setIterations(iterations)
-        while self.i < j:
 
-            try:
-                self.likePost()
-                if self.i % 20 == 0:
-                    self.commentPost()
-                next = self.browser.find_element_by_class_name("coreSpriteRightPaginationArrow")
-                next.click()
-                self.i += 1
-            except:
-                self.reloadBrowser()
 
 
     def closeBrowser(self):
@@ -81,7 +77,7 @@ class Automation:
 
     def reloadBrowser(self):
         self.browser.refresh()
-        self.image_reload_helper = self.i
+        self.image_reload_helper = self.i + 1
         if self.image_reload_helper < 9:
             self.image_reload_helper = 9
         self.navigation()
@@ -121,7 +117,8 @@ class Automation:
         return 0
 
     def getIterations(self):
-        return self.iterations
+
+        return self.j
 
     def setComments(self):
         self.comments = []
@@ -135,6 +132,13 @@ class Automation:
         number = random.randint(0, len(self.comments) - 1)
         return self.comments[number]
 
+    def setBoolComment(self, bool):
+        self.boolComment = bool
+
+    def getBoolComment(self):
+        return self.boolComment
+
+
     def tooString(self):
-        return "this is a working instance"
+        return "this is a working instance of " + self.getUsername()
 
---
+++ AutomationGUI.py
@@ -0,0 +1,133 @@
+from tkinter import *
+from Automate import Automation
+from threading import Thread
+from PyQt5.QtCore import *
+from time import sleep
+
+LARGE_FONT = ("Verdana", 12)
+NORM_FONT = ("Verdana", 10)
+SMALL_FONT = ("Verdana", 8)
+
+def popupmsg_error(msg):
+    popup = Tk()
+
+    def leavemini():
+        popup.destroy()
+
+    popup.wm_title("ERROR!")
+    label = Label(popup, text=msg, font=NORM_FONT)
+    label.pack(side="top", fill="x", pady= 10)
+    B1 = Button(popup, text="Okay", command=leavemini)
+    B1.pack()
+    popup.mainloop()
+
+
+class AutomateGUI(QObject):
+
+    def __init__(self, master, parent=None):
+        super(AutomateGUI, self).__init__(parent)
+        frame = Frame(master)
+        frame.grid()
+        self.defaultsleep = IntVar(value=30, name="30")
+        self.defaultIterations = IntVar(value=250, name="250")
+        #
+        self.entry_user = Entry(root)
+        self.entry_password = Entry(root)
+        self.entry_hashtag = Entry(root)
+        self.entry_timesleep = Entry(root)
+        self.entry_timesleep.insert(END, self.defaultsleep)
+        self.entry_iterations = Entry(root)
+        self.entry_iterations.insert(END, self.defaultIterations)
+
+        self.label_username = Label(root, text="Username: ")
+        self.label_password = Label(root, text="Password: ")
+        self.label_hashtag = Label(root, text="Target hashtag: ")
+        self.label_timesleep = Label(root, text="| Time Between Likes:")
+        self.label_iterations = Label(root, text="Iterations:")
+        self.persistLogin = Checkbutton(root, text="Stay Logged in?*")
+        self.button_launch = Button(root, text="Launch", command=self.processLaunch)
+
+
+
+        self.label_username.grid(row=0, sticky=E)
+        self.label_password.grid(row=0, column=2, sticky=E)
+        self.persistLogin.grid(row=1, columnspan=3)
+        self.label_timesleep.grid(row=1, column=2, sticky=E)
+        self.label_iterations.grid(row=2, column=2, sticky=E)
+        self.label_hashtag.grid(row=3, sticky=E)
+        self.button_launch.grid(row=3, column=3)
+
+        self.entry_user.grid(row=0, column=1)
+        self.entry_password.grid(row=0, column=3)
+        self.entry_hashtag.grid(row=3, column=1)
+        self.entry_timesleep.grid(row=1, column=3)
+        self.entry_iterations.grid(row=2,column=3)
+
+
+    def processLaunch(self):
+        # Initialize worker thread
+        self.WorkerThread = WorkerThread(username=self.entry_user.get(), password=self.entry_password.get(), hashtag=self.entry_hashtag.get(), timesleep=self.entry_timesleep.get(), iterations=self.entry_iterations.get())
+        self.WorkerThread.start()
+
+
+class WorkerThread(Thread):
+
+    def __init__(self, username, password, hashtag, timesleep, iterations, parent=None):
+        super(WorkerThread, self).__init__(parent)
+
+        self.instance = Automation(username=username, password=password, timesleep=timesleep, iterations=iterations)
+        self.hashtag = hashtag
+        self.i = 0
+
+    def run(self):
+
+        self.instance.setUrl(self.hashtag)
+
+        def loginInstace(instance):
+            try:
+                instance.login()
+            except:
+                instance.reloadBrowser()
+                sleep(3)
+                loginInstace(instance)
+
+        def navigateInstance(instance):
+            try:
+                instance.navigation()
+            except:
+                msg = "Chrome Was Unreachable and will be reloaded.  If persistant restart application and report bug to Matt"
+                popupmsg_error(msg)
+                sleep(3)
+                navigateInstance(instance)
+
+        def likePostInstance(instance):
+            try:
+                instance.likePost()
+            except:
+                instance_next = instance.browser.find_element_by_class_name("coreSpriteRightPaginationArrow")
+                instance_next.click()
+                likePostInstance(instance)
+
+        # this is where the action starts
+        
+        loginInstace(self.instance)
+        navigateInstance(self.instance)
+        while self.i < self.instance.getIterations():
+            sleep(2)
+            likePostInstance(self.instance)
+
+            instance_next = self.instance.browser.find_element_by_class_name("coreSpriteRightPaginationArrow")
+            instance_next.click()
+            self.i += 1
+            print("I am on number", format(self.i, "d"), ". That's", format(self.i / self.instance.getIterations() * 100, "0.2f"), "% complete.")
+
+
+
+
+root = Tk(className=" Automate Instagram")
+
+automate = AutomateGUI(root)
+
+
+
+root.mainloop()
